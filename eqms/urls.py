af29408691f3ab9de3ba1ed0e7475db163eb51bf
Otuoze <otuozeahmedrufai@gmail.com>
2023-03-27 15:27:50 +0100

    completed ui

--- eqms/urls.py
+++ eqms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, welcome, equipment_type, tag_details, oem_details
+from .views import index, welcome, equipment_type, tag_details, oem_details,equipment_data
 
 urlpatterns = [
     path("index/", index, name="home"),
     path("equipment/type/", equipment_type),
-    path("tag/data/", tag_details),
-    path("oem/data/", oem_details),
+    path("equipment/count/", tag_details),
+    path("oem/sources/", oem_details),
+    path("equipment/data/", equipment_data),
     path("", welcome)
 ]
--- eqms/views.py
+++ eqms/views.py
@@ -8,10 +8,13 @@ def equipment_type(request):
     return render(request, "equipment-type.html")
 
 def welcome(request):
-    return render(request, "ui-buttons.html")
+    return render(request, "welcome.html")
 
 def tag_details(request):
-    return render(request, "bootstrap-tables.html")
+    return render(request, "tag_details.html")
 
 def oem_details(request):
     return render(request, "oem_details.html")
+
+def equipment_data(request):
+    return render(request, "equipment_data.html")
